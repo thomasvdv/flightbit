deeac780e85e4b8e78dc1f25f1869546be109ea0
Thomas Van de Velde <thomasevdv@gmail.com>
2015-11-08 10:40:34 -0800

    Base code structure

---
+++ OLC/flight/flight/items.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class FlightItem(scrapy.Item):
+    # define the fields for your item here like:
+    airport_id = scrapy.Field()
+    date = scrapy.Field()
+    points = scrapy.Field()
+    pilot_name = scrapy.Field()
+    pilot_id = scrapy.Field()
+    km = scrapy.Field()
+    kph = scrapy.Field()
+    club = scrapy.Field()
+    aircraft = scrapy.Field()
+    start = scrapy.Field()
+    end = scrapy.Field()
+    flight_id = scrapy.Field()
+    pass
