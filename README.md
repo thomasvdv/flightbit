35e7fd862677746c05991135f614ab494dd5bf5f
Thomas Van de Velde <thomasevdv@gmail.com>
2016-12-21 20:22:58 +0100

    Added sounding analysis

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ grib_ls -m -l 47.683900,-120.935794,1 -p paramId,name,level,time rap_130_2012082
 
 RAP Archive:
 https://www.ncdc.noaa.gov/data-access/model-data/model-datasets/rapid-refresh-rap
+
+Thermal near Ephrata:
+===
+68de910c-ff55-4be7-b163-0d46e8a94c97	55584744	-119.56293333337	47.3514166666813	1425	1.94723618090453	775	2013-06-01 21:25:42
+
+
+http://www.onlinecontest.org/olc-2.0/gliding/flightinfo.html?flightId=55584744
---
+++ analysis/csv_thermal.py
@@ -0,0 +1,81 @@
+import xcsoar
+import simplekml
+import csv
+import uuid
+from os import listdir
+from os.path import isfile, join
+
+
+def processThermals(in_dir, out_dir):
+
+    my_thermals = [f for f in listdir(in_dir) if isfile(join(in_dir, f))]
+
+    try:
+        f = open('{}/thermals.csv'.format(out_dir), 'wt')
+        writer = csv.writer(f)
+        writer.writerow(('thermal_id','flight_id', 'longitude', 'latitude', 'heigth', 'vario', 'alt_diff', 'time'))
+
+        ef = open('{}/errors.csv'.format(out_dir), 'wt')
+        ef_writer = csv.writer(ef)
+
+        for my_igc in my_thermals:
+            processThermal(join(in_dir, my_igc), out_dir, writer, ef_writer)
+
+    finally:
+        f.close()
+        ef.close()
+
+
+def processThermal(my_igc, out_dir, writer, ef_writer):
+    flight_id = my_igc.split('/')[-1].split('.')[0]
+
+    print("Processing flight {}...".format(flight_id))
+
+    flight = xcsoar.Flight(my_igc)
+
+    times = flight.times()
+
+    if len(times) == 0:
+        print("Skipping file {}!".format(flight_id))
+        ef_writer.writerow((str(flight_id)))
+        return
+
+    for dtime in times:
+        takeoff = dtime['takeoff']
+        try:
+            release = dtime['release']
+        except KeyError:
+            print("No release detected!")
+            release = dtime['takeoff']
+        landing = dtime['landing']
+
+    analysis = flight.analyse(takeoff=takeoff['time'],
+                              scoring_start=release['time'],
+                              scoring_end=landing['time'],
+                              landing=landing['time'])
+
+    fixes = flight.path(release['time'], landing['time'])
+
+    for phase in analysis['phases']:
+        bottom_lon = 0
+        bottom_lat = 0
+        bottom_heigth = 0
+        for fix in fixes:
+            if fix[0] == phase['start_time']:
+                bottom_fix = fix[2]
+                bottom_lon = bottom_fix['longitude']
+                bottom_lat = bottom_fix['latitude']
+                bottom_heigth = fix[4]
+            if fix[0] == phase['end_time']:
+                top_fix = fix[2]
+                top_lon = top_fix['longitude']
+                top_lat = top_fix['latitude']
+                top_heigth = fix[4]
+                if bottom_lat < 49 and top_lat < 49 and phase['alt_diff'] > 305 and phase['vario'] < 8 and phase['vario'] > 0 and bottom_heigth > 0:
+                    if phase['type'] == 'circling':
+                        thermal_id = str(uuid.uuid4())
+                        writer.writerow((thermal_id, flight_id, bottom_lon, bottom_lat, bottom_heigth, phase['vario'], phase['alt_diff'],
+                                         phase['start_time'].isoformat(' ')))
+
+if __name__ == '__main__':
+    processIGCs("/home/thomasvdv/OLC/IGC", "/home/thomasvdv/OLC/CSV")
