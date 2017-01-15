a7b0cf34992edb35130fbcb0f55e5faa69c59301
Thomas Van de Velde <thomasevdv@gmail.com>
2017-01-15 07:33:23 -0800

    Changed the spider to be stand-alone and fixed an issue with the GRIB date ranges

--- OLC/flight/flight/pipelines.py
+++ OLC/flight/flight/pipelines.py
@@ -1,4 +1,3 @@
-from scrapy.contrib.exporter import CsvItemExporter
 # -*- coding: utf-8 -*-
 
 # Define your item pipelines here
@@ -8,17 +7,5 @@ from scrapy.contrib.exporter import CsvItemExporter
 
 
 class FlightPipeline(object):
-
-    def open_spider(self, spider):
-        self.file = open('output.csv', 'w+b')
-        self.exporter = CsvItemExporter(self.file)
-        self.exporter.start_exporting()
-
-    def close_spider(self, spider):
-        self.exporter.finish_exporting()
-        self.file.close()
-
-
     def process_item(self, item, spider):
-        self.exporter.export_item(item)
         return item
