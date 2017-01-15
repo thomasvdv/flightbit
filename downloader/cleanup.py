6e55f93b037587dd993f3b743f64055f41096dd5
Thomas Van de Velde <thomasevdv@gmail.com>
2017-01-15 08:27:11 -0800

    Added a cleanup script for corrupt IGC files.

---
+++ downloader/cleanup.py
@@ -0,0 +1,11 @@
+import csv
+from os.path import expanduser
+import os
+
+home = expanduser("~")
+
+with open(home+'/OLC/CSV/errors.csv', 'rb') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print row[0]
+        os.remove(home+'/OLC/IGC/'+row[0]+'.igc')
